9b4cbf8f19f2acc62b3abd9ea601f19be1b0b68d
Douglas Chen <dpchen@mit.edu>
2017-02-07 17:55:03 -0500

    Run both with and without logging

--- log-tbox-primary.cc
+++ log-tbox-primary.cc
@@ -16,11 +16,21 @@ int start_port;
 struct ThreadArgs {
     int id;
     TBox<int> *fs;
+    bool enable_logging;
 };
 
 void *test_multithreaded_int_worker(void *argptr) {
     ThreadArgs &args = *(ThreadArgs *) argptr;
     TThread::set_id(args.id);
+
+    /*
+    cpu_set_t cpuset;
+    CPU_ZERO(&cpuset);
+    CPU_SET(args.id, &cpuset);
+    CPU_SET(args.id + 32, &cpuset);
+    sched_setaffinity(0, sizeof(cpuset), &cpuset);
+    */
+
     TBox<int> *fs = args.fs;
     int val = 0;
     for (int i = 0; i < niters; i++) {
@@ -30,16 +40,18 @@ void *test_multithreaded_int_worker(void *argptr) {
             val++;
         }
     }
-    Transaction::flush_log_batch();
+    if (args.enable_logging)
+        Transaction::flush_log_batch();
     return nullptr;
 }
 
-void test_multithreaded_int() {
+void test_multithreaded_int(bool enable_logging) {
     usleep(startup_delay);
     std::vector<TBox<int>> fs(nthreads * txnsize);
     for (unsigned i = 0; i < fs.size(); i++)
         Transaction::register_object(fs[i], i);
-    assert(Transaction::init_logging(nthreads, {backup_host}, start_port) == 0);
+    if (enable_logging)
+        assert(Transaction::init_logging(nthreads, {backup_host}, start_port) == 0);
 
     pthread_t thrs[nthreads];
     ThreadArgs args[nthreads];
@@ -49,7 +61,7 @@ void test_multithreaded_int() {
     hc::time_point time_start = hc::now();
 
     for (int i = 0; i < nthreads; i++) {
-        args[i] = { .id = i, .fs = fs.data() };
+        args[i] = { .id = i, .fs = fs.data(), .enable_logging = enable_logging };
         pthread_create(&thrs[i], nullptr, test_multithreaded_int_worker, (void *) &args[i]);
     }
     for (int i = 0; i < nthreads; i++)
@@ -57,18 +69,24 @@ void test_multithreaded_int() {
 
     hc::time_point time_end = hc::now();
 
-    Transaction::stop_logging();
+    if (enable_logging)
+        Transaction::stop_logging();
     Transaction::clear_registered_objects();
     printf("PRIMARY PASS: %s()\n", __FUNCTION__);
 
     int64_t us = std::chrono::duration_cast<std::chrono::microseconds>(time_end - time_start).count();
     log_stats_t agg;
-    for (int i = 0; i < nthreads; i++) {
-        log_stats_t &s = Transaction::tinfo[i].log_stats;
-        agg.bytes += s.bytes;
-        agg.bufs += s.bufs;
-        agg.ents += s.ents;
-        agg.txns += s.txns;
+    if (enable_logging) {
+        for (int i = 0; i < nthreads; i++) {
+            log_stats_t &s = Transaction::tinfo[i].log_stats;
+            agg.bytes += s.bytes;
+            agg.bufs += s.bufs;
+            agg.ents += s.ents;
+            agg.txns += s.txns;
+        }
+    } else {
+        agg.ents = txnsize * nthreads * niters;
+        agg.txns = nthreads * niters;
     }
 
     printf("MB=%.2f bufs=%llu ents=%llu txns=%llu\n", agg.bytes/1.0e6, agg.bufs, agg.ents, agg.txns);
@@ -92,6 +110,7 @@ int main(int argc, char **argv) {
 
     TThread::set_id(0);
     Transaction::debug_txn_log = false;
-    test_multithreaded_int();
+    test_multithreaded_int(false);
+    test_multithreaded_int(true);
     return 0;
 }
